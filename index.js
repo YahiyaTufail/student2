43a99ba977a8d1a0554b6bda78cdeffaba3051d3
YahiyaTufail <121566555+YahiyaTufail@users.noreply.github.com>
2023-05-13 19:21:47 +0500

    Assignment1
    color changing

---
+++ index.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById("square").style.backgroundColor = color;
+}
